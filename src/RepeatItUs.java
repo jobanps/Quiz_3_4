183fc0335710f222f75d1e9ceb348d5730bf43f1
Joban <Joban@LAPTOP-6GDEOP3F>
2019-05-28 16:26:32 -0400

    Question 3 Commit

--- src/RepeatItUs.java
+++ src/RepeatItUs.java
@@ -12,7 +12,28 @@ public class RepeatItUs {
 		//Q2: The last 15 characters in the string
 		int startIndex = sentenceLength - 15; // To get index of 15th char from end 'string size - 15'
 		String last15Char = sentence.substring(startIndex);
-		System.out.println("Last 15 Characters in sentence : " + last15Char);				
+		System.out.println("Last 15 Characters in sentence : " + last15Char);		
+		
+		//Q3 : Number of times p and c repeat in sentence
+		int pCount = 0;
+		int cCount = 0;
+		
+		String[] splitSentence = sentence.split("");
+		
+		for(int i = 0 ; i < splitSentence.length ; i++) {
+			
+			if(splitSentence[i].equals("c")) {
+				cCount++;
+			}
+			
+			if(splitSentence[i].equals("p")) {
+				pCount++;
+			}
+		}
+		
+		System.out.println("Number of times for c : " + cCount);
+		System.out.println("Number of times for p : " + pCount);
+
 	
 	}
 
