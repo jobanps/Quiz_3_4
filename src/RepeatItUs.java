95c6b2d7f49bb9b69b1ec2c731122bb7becb008f
Joban <Joban@LAPTOP-6GDEOP3F>
2019-05-28 16:11:01 -0400

    Q1 Commit

--- src/RepeatItUs.java
+++ src/RepeatItUs.java
@@ -3,6 +3,13 @@ public class RepeatItUs {
 	
 	public static void main(String[] args) {
 		
+		String sentence = "copyandpastecopyandpastecopyandpastecopyandpastecopyandpastecopyandpastecommitandpushcommitandpushcommitandpushcommitandpushpushcccccommmitttsss";
+		
+		//Q1: The total number of characters in the sentence
+		int sentenceLength = sentence.length();
+		System.out.println("Total number of characters in the sentence : " + sentenceLength);
+				
+	
 	}
 
 }
