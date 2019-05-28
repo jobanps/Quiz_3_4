189266a41d73126db42b18bf6cbae65f72bac8b0
Joban <Joban@LAPTOP-6GDEOP3F>
2019-05-28 16:17:21 -0400

    Question 2 Commit

--- src/RepeatItUs.java
+++ src/RepeatItUs.java
@@ -8,7 +8,11 @@ public class RepeatItUs {
 		//Q1: The total number of characters in the sentence
 		int sentenceLength = sentence.length();
 		System.out.println("Total number of characters in the sentence : " + sentenceLength);
-				
+		
+		//Q2: The last 15 characters in the string
+		int startIndex = sentenceLength - 15; // To get index of 15th char from end 'string size - 15'
+		String last15Char = sentence.substring(startIndex);
+		System.out.println("Last 15 Characters in sentence : " + last15Char);				
 	
 	}
 
